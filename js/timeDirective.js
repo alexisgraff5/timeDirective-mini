489e4e50570d813d8277ef6916e83a1705520310
alexisgraff5 <lex.graff@gmail.com>
2016-11-01 10:34:16 -0600

    completed the mini project

--- js/timeDirective.js
+++ js/timeDirective.js
@@ -1,5 +1,10 @@
 angular.module('timeApp').directive('showTime', function(){
-return {
-
+  return {
+    restrict: 'E',
+    template: '<div> The current time is {{time}} </div>',
+    link: function(scope, element, attrs) {
+      var currentTime = new Date();
+      scope.time = currentTime.toString();
+    }
 };
 });
