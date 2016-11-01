2530bb67f32b93d32719f410402836c5b0eebaf0
alexisgraff5 <lex.graff@gmail.com>
2016-11-01 11:54:13 -0600

    added {{name}} in template

--- js/timeDirective.js
+++ js/timeDirective.js
@@ -1,7 +1,7 @@
 angular.module('timeApp').directive('showTime', function(){
   return {
     restrict: 'E',
-    template: '<div> The current time is {{time}} </div>',
+    template: '<div> Hello {{name}}, The current time is {{time}} </div>',
     link: function(scope, element, attrs) {
       var currentTime = new Date();
       scope.time = currentTime.toString();
